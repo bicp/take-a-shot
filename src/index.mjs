a3b6028f6c7ac5a1628044ebe4d13b8fe2e76c3c
Bianca Pimenta <bi.cp1802@gmail.com>
2022-01-06 23:38:54 +0100

    add deploy

--- src/index.mjs
+++ src/index.mjs
@@ -6,9 +6,12 @@ const startButton = document.getElementById("start-btn");
 startButton.addEventListener("click", startGame);
 
 const questionContainerElement = document.getElementById("question-container");
+const explanationElement = document.getElementById("explanation");
 
 const ansBtnTrue = document.getElementById("btntrue");
 const ansBtnFalse = document.getElementById("btnfalse");
+const nextElement = document.getElementById("next-btn");
+
 ansBtnTrue.addEventListener("click", function () {
   if (question.fact === true) {
     ansBtnTrue.classList.add("correct");
@@ -16,16 +19,51 @@ ansBtnTrue.addEventListener("click", function () {
     ansBtnTrue.classList.add("wrong");
   }
 });
-ansBtnFalse.addEventListener("click", function () {});
+
+ansBtnTrue.addEventListener("click", function () {
+  const explanationElement = document.getElementById("explanation");
+  explanationElement.innerText = question.explanation;
+});
+
+ansBtnFalse.addEventListener("click", function () {
+  explanationElement.innerText = question.explanation;
+});
+
+ansBtnFalse.addEventListener("click", function () {
+  if (question.fact === false) {
+    ansBtnFalse.classList.add("correct");
+  } else {
+    ansBtnFalse.classList.add("wrong");
+  }
+});
+
+ansBtnTrue.addEventListener("click", function () {
+  nextElement.classList.remove("hide");
+});
+
+ansBtnFalse.addEventListener("click", function () {
+  nextElement.classList.remove("hide");
+});
+
+nextElement.addEventListener("click", function () {
+  startGame();
+});
 
 function startGame() {
   console.log("Started");
   startButton.classList.add("hide");
   questionContainerElement.classList.remove("hide");
+  explanationElement.innerText = "";
+  ansBtnTrue.classList.remove("correct");
+  ansBtnTrue.classList.remove("wrong");
+  ansBtnFalse.classList.remove("correct");
+  ansBtnFalse.classList.remove("wrong");
+
   setNextQuestion();
 }
 
 function setNextQuestion() {
+  question = questions[Math.floor(Math.random() * questions.length)];
   const questionElement = document.getElementById("question");
   questionElement.innerText = question.claim;
 }
